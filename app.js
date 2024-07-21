fc8d4b6b6105e2e5d5821052d25063e80324739f
Mohammed-Parvez <mdparvezbyahatti@gmail.com>
2024-07-21 23:00:17 +0530

    Functional-Calculator-JS

---
+++ app.js
@@ -0,0 +1,70 @@
+const inputPre = document.querySelector(".input-pre");
+const inputNex = document.querySelector(".input-nex");
+const numbers = document.querySelectorAll(".btn");
+const operators = document.querySelectorAll(".operator");
+const equalBtn = document.querySelector(".equal");
+const clearBtn = document.querySelector(".clear");
+const deleteBtn = document.querySelector(".delete");
+
+// Number Inputs
+
+numbers.forEach((e) => {
+  e.addEventListener("click", () => {
+    inputNex.value += e.textContent;
+  });
+});
+
+// Operator Inputs
+operators.forEach((e) => {
+  e.addEventListener("click", () => {
+    inputNex.value += e.textContent;
+  });
+});
+
+// Evaluate Function
+equalBtn.addEventListener("click", () => {
+  try {
+    evaluate = eval(inputNex.value);
+    inputPre.value = evaluate;
+  } catch (error) {
+    inputPre.value = "Error";
+  }
+});
+
+// Clear Function
+clearBtn.addEventListener("click", () => {
+  inputNex.value = "";
+  inputPre.value = "";
+});
+
+// Delete Value Function
+deleteBtn.addEventListener("click", () => {
+  inputNex.value = inputNex.value.slice(0, -1);
+});
+
+// Disable Function Evokes when Error Occurs
+// function disable() {
+//     numbers.forEach((e) => {
+//         e.disabled = true;
+//     })
+
+//     operators.forEach((e) => {
+//         e.disabled = true;
+//     })
+
+//     equalBtn.disabled = true;
+//     deleteBtn.disabled = true;
+// }
+
+// function enable() {
+//     numbers.forEach((e) => {
+//         e.disabled = false;
+//     })
+
+//     operators.forEach((e) => {
+//         e.disabled = false;
+//     })
+
+//     equalBtn.disabled = false;
+//     deleteBtn.disabled = false;
+// }
